5bd2d63a2331a200d31ac890c67f16ba5c3f09bd
Marcel Haerry <haerry@puzzle.ch>
2008-04-30 13:47:31 +0200

    took define out of class

--- manifests/init.pp
+++ manifests/init.pp
@@ -81,18 +81,6 @@ class ntp::base {
 
 	}
 
-
-	# public
-	define upstream_server($server_options = 'iburst') {
-		ntp::add_config { "server_${name}":
-			content => "server ${name} ${server_options}",
-			type => "server",
-		}
-		# This will need the ability to collect exported defines
-		# currently this is worked around by reading /etc/ntp*conf via a fact
-		# case $name { $fqdn: { debug ("${fqdn}: Ignoring get_time_from for self") } default: { munin_ntp { $name: } } }
-	}
-
 	# private
 	# Installs a munin plugin and configures it for a given host
 	define munin_plugin() {
@@ -109,6 +97,17 @@ class ntp::base {
 		}
 	}
 
+}
+
+# public
+define ntp::upstream_server($server_options = 'iburst') {
+    ntp::add_config { "server_${name}":
+	    content => "server ${name} ${server_options}",
+		type => "server",
+	}
+	# This will need the ability to collect exported defines
+	# currently this is worked around by reading /etc/ntp*conf via a fact
+	# case $name { $fqdn: { debug ("${fqdn}: Ignoring get_time_from for self") } default: { munin_ntp { $name: } } }
 }
 
 # this is a server, connect to the specified upstreams
